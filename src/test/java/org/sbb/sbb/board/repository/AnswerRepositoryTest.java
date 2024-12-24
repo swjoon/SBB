66c86af25a183b8147d2cb6f576a6ba2dfb664bb
swjoon <jetkid1446@gmail.com>
2024-12-24 12:31:14 +0900

    3-14 answer paging test

--- src/test/java/org/sbb/sbb/board/repository/AnswerRepositoryTest.java
+++ src/test/java/org/sbb/sbb/board/repository/AnswerRepositoryTest.java
@@ -1,4 +1,4 @@
-package org.sbb.sbb.repository;
+package org.sbb.sbb.board.repository;
 
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,12 +10,18 @@ import org.sbb.sbb.board.question.domain.Question;
 import org.sbb.sbb.config.dummy.DummyObject;
 import org.sbb.sbb.board.question.repository.QuestionRepository;
 import org.sbb.sbb.user.domain.User;
+import org.sbb.sbb.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,6 +31,9 @@ public class AnswerRepositoryTest extends DummyObject {
 
     @Autowired
     private EntityManager em;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private AnswerRepository answerRepository;
@@ -40,6 +49,7 @@ public class AnswerRepositoryTest extends DummyObject {
     }
 
     private void autoIncrementReset() {
+        em.createNativeQuery("ALTER TABLE users ALTER COLUMN id RESTART WITH 1").executeUpdate();
         em.createNativeQuery("ALTER TABLE answer ALTER COLUMN id RESTART WITH 1").executeUpdate();
         em.createNativeQuery("ALTER TABLE question ALTER COLUMN id RESTART WITH 1").executeUpdate();
     }
@@ -48,13 +58,13 @@ public class AnswerRepositoryTest extends DummyObject {
         User user = DummyObject.newUser("testuser1", "test1", "<EMAIL>");
         Question q1 = DummyObject.newQuestion("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.", user);
         Question q2 = DummyObject.newQuestion("스프링부트 모델 질문입니다.", "id는 자동으로 생성되나요?", user);
-
-        Answer a1 = DummyObject.newAnswer("네 자동으로 생성됩니다.", user, q2);
-
+        userRepository.save(user);
         questionRepository.save(q1);
         questionRepository.save(q2);
-
-        answerRepository.save(a1);
+        for (int i = 1; i <= 3; i++) {
+            Answer answer = DummyObject.newAnswer(i + "", user, q2);
+            answerRepository.save(answer);
+        }
     }
 
     @Test
@@ -89,7 +99,40 @@ public class AnswerRepositoryTest extends DummyObject {
 
         List<Answer> answerList = q.getAnswerList();
 
-        assertEquals(1, answerList.size());
-        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+        assertEquals(3, answerList.size());
+        assertEquals(1 + "", answerList.get(0).getContent());
+    }
+
+    @Test
+    @DisplayName("기본정렬 페이징")
+    void getAnswerPageDefault() {
+        List<Sort.Order> list = new ArrayList<>();
+        list.add(new Sort.Order(Sort.Direction.DESC, "id"));
+        Pageable pageable = PageRequest.of(0, 2, Sort.by(list));
+
+        Page<Answer> answerPage = answerRepository.findAnswersByQuestionId(2, pageable);
+
+        System.out.println(answerPage.getTotalElements());
+
+        assertEquals(3 + "", answerPage.getContent().get(0).getContent());
+        assertEquals(2, answerPage.getContent().size());
+    }
+
+    @Test
+    @DisplayName("추천수정렬 페이징")
+    void getAnswerPageVote() {
+        User user = userRepository.findById(1L).get();
+        Answer answer = answerRepository.findById(1).get();
+        answer.getVoter().add(user);
+
+        List<Sort.Order> list = new ArrayList<>();
+        list.add(new Sort.Order(Sort.Direction.DESC, "voter"));
+        Pageable pageable = PageRequest.of(0, 2, Sort.by(list));
+
+        Page<Answer> answerPage = answerRepository.findAnswersByQuestionId(2, pageable);
+
+        System.out.println(answerPage.getContent().get(0).getContent());
+
+        assertEquals(1 + "", answerPage.getContent().get(0).getContent());
     }
 }
--- src/test/java/org/sbb/sbb/board/repository/QuestionRepositoryTest.java
+++ src/test/java/org/sbb/sbb/board/repository/QuestionRepositoryTest.java
@@ -1,4 +1,4 @@
-package org.sbb.sbb.repository;
+package org.sbb.sbb.board.repository;
 
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
